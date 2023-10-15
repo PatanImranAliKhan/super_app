3eed0f198ba5f3895cc75e51e7f17e59e0bd9d62
Patan Khan <patan_khan@bonify.de>
2023-10-16 00:26:12 +0530

    added registration page

---
+++ src/Components/Register.jsx
@@ -0,0 +1,184 @@
+import React, { useState } from 'react'
+import './register.css'
+import RegisterForm from './RegisterForm';
+
+const Register = () => {
+
+  const [userDetails, setUserDetails] = useState({
+    name: "",
+    username: "",
+    email: "",
+    mobile: "",
+    confirmation: false
+  });
+
+  const [userDetailsError, setUserDetailsError] = useState({
+    name: "",
+    username: "",
+    email: "",
+    mobile: "",
+    confirmation: ""
+  });
+
+
+  const handleOnchangeEvent = (e) => {
+    let name = e.target.name, value = e.target.value;
+    if (name === "confirmation") {
+      value = !userDetails.confirmation;
+    }
+    setUserDetails({
+      ...userDetails,
+      [name]: value
+    })
+  }
+
+  const handleSubmitEvent = (e) => {
+    e.preventDefault();
+    let v1=false, v2=false,v3=false,v4=false,v5=false;
+    v1=validateEmail();
+    v2=validateMobileNumber();
+    v3=validateName();
+    v4=validateUserName();
+    v5=validateConfirmation();
+    if(v1 && v2 && v3 && v4 && v5) {
+      localStorage.setItem("user", userDetails);
+    }
+  }
+
+  const validateConfirmation = (confirmation = userDetails.confirmation) => {
+    if (confirmation === false) {
+      setUserDetailsError((prev) => {
+        return {
+          ...prev,
+          confirmation: "Check this box if you want to proceed"
+        }
+      })
+      return false;
+    } else {
+      setUserDetailsError((prev) => {
+        return {
+          ...prev,
+          confirmation: ""
+        }
+      })
+    }
+    return true;
+  }
+
+  const validateMobileNumber = (mobile = userDetails.mobile) => {
+    if (mobile === "") {
+      setUserDetailsError((prev) => {
+        return {
+          ...prev,
+          mobile: "Field is required"
+        }
+      })
+      return false;
+    } else if (mobile.length !== 10) {
+      setUserDetailsError((prev) => {
+        return {
+          ...prev,
+          mobile: "Mobile number must be 10 digits"
+        }
+      })
+      return false;
+    } else {
+      setUserDetailsError((prev) => {
+        return {
+          ...prev,
+          mobile: ""
+        }
+      })
+    }
+    return true;
+  }
+
+  const validateName = (name = userDetails.name) => {
+    if (name === "") {
+      setUserDetailsError((prev) => {
+        return {
+          ...prev,
+          name: "Field is required"
+        }
+      })
+      return false;
+    } else {
+      setUserDetailsError((prev) => {
+        return {
+          ...prev,
+          name: ""
+        }
+      })
+    }
+    return true;
+  }
+
+  const validateUserName = (username = userDetails.username) => {
+    if (username === "") {
+      setUserDetailsError((prev) => {
+        return {
+          ...prev,
+          username: "Field is required"
+        }
+      })
+      return false;
+    } else {
+      setUserDetailsError((prev) => {
+        return {
+          ...prev,
+          username: ""
+        }
+      })
+    }
+    return true;
+  }
+
+  const verifyEmail = (email) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      )
+  }
+
+  const validateEmail = (email = userDetails.email) => {
+    if (email === "") {
+      setUserDetailsError((prev) => {
+        return {
+          ...prev,
+          email: "Field is required"
+        }
+      })
+      return false;
+    } else if (!verifyEmail(email)) {
+      setUserDetailsError((prev) => {
+        return {
+          ...prev,
+          email: "This Field must be email"
+        }
+      })
+      return false;
+    } else {
+      setUserDetailsError((prev) => {
+        return {
+          ...prev,
+          email: ""
+        }
+      })
+    }
+    return true;
+  }
+
+  return (
+    <div>
+      <RegisterForm
+        userDetails={userDetails}
+        userDetailsError={userDetailsError}
+        handleSubmitEvent={handleSubmitEvent}
+        handleOnchangeEvent={handleOnchangeEvent}
+      />
+    </div>
+  )
+}
+
+export default Register
---
+++ src/Components/RegisterForm.jsx
@@ -0,0 +1,96 @@
+import React from 'react'
+
+const RegisterForm = ({ userDetails, userDetailsError, handleSubmitEvent, handleOnchangeEvent }) => {
+    return (
+        <div>
+            <p className='imageDesc'>Discover new things on Superapp</p>
+            <div className='register'>
+                <img src="/assets/register.png" alt="Register_Image" />
+                <div className='createAccount'>
+                    <form onSubmit={(e) => { handleSubmitEvent(e) }}>
+                        <div className='heading'>
+                            <h1>Super app</h1>
+                            <p>Create your own account</p>
+                        </div>
+                        <div className='inputValues'>
+                            <div className='inputDiv'>
+                                <input type="text" name='name' placeholder='Name'
+                                    style={{ border: userDetailsError.name ? '1px solid' : 'none', borderColor: 'red' }}
+                                    value={userDetails.name} onChange={(e) => { handleOnchangeEvent(e) }} />
+                                <span>
+                                    {
+                                        userDetailsError.name && (
+                                            <span className='error'>{userDetailsError.name}</span>
+                                        )
+                                    }
+                                </span>
+
+                            </div>
+
+                            <div className='inputDiv'>
+                                <input type="text" name='username' placeholder='UserName'
+                                    style={{ border: userDetailsError.username ? '1px solid' : 'none', borderColor: 'red' }}
+                                    value={userDetails.username} onChange={(e) => { handleOnchangeEvent(e) }} />
+                                <span>
+                                    {
+                                        userDetailsError.username && (
+                                            <span className='error'>{userDetailsError.username}</span>
+                                        )
+                                    }
+                                </span>
+                            </div>
+
+                            <div className='inputDiv'>
+                                <input type="email" name='email' placeholder='Email'
+                                    style={{ border: userDetailsError.email ? '1px solid' : 'none', borderColor: 'red' }}
+                                    value={userDetails.email} onChange={(e) => { handleOnchangeEvent(e) }} />
+                                <span>
+                                    {
+                                        userDetailsError.email && (
+                                            <span className='error'>{userDetailsError.email}</span>
+                                        )
+                                    }
+                                </span>
+                            </div>
+
+                            <div className='inputDiv'>
+                                <input type="text" name='mobile' placeholder='Mobile'
+                                    style={{ border: userDetailsError.mobile ? '1px solid' : 'none', borderColor: 'red' }}
+                                    value={userDetails.mobile} onChange={(e) => { handleOnchangeEvent(e) }} />
+                                <span>
+                                    {
+                                        userDetailsError.mobile && (
+                                            <span className='error'>{userDetailsError.mobile}</span>
+                                        )
+                                    }
+                                </span>
+                            </div>
+                        </div>
+                        <div>
+                            <div className='checkboxContent'>
+                                <input type="checkbox" name='confirmation' onChange={(e) => { handleOnchangeEvent(e) }} />
+                                <span>Share my registration data with Superapp</span>
+                            </div>
+                            <br />
+                            <span>
+                                {
+                                    userDetailsError.confirmation && (
+                                        <span className='error'>{userDetailsError.confirmation}</span>
+                                    )
+                                }
+                            </span>
+                        </div>
+                        <button type='submit'>SIGN UP</button>
+                        <div className='policy'>
+                            <p>By clicking on Sign up. you agree to Superapp <span className='highlight'>Terms and Conditions of Use</span></p>
+                            <p>To learn more about how Superapp collects, uses, shares and protects your personal
+                                data please head Superapp <span className='highlight'>Privacy Policy</span></p>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default RegisterForm
